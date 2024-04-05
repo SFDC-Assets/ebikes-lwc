149ec35b0f8c144cf6a1c1caca14e2bab61658c5
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-04-05 12:14:02 -0700

    Create orgInit.sh

---
+++ orgInit.sh
@@ -0,0 +1,8 @@
+sfdx shane:org:create -f config/project-scratch-def.json -d 5 -s --wait 60 --userprefix ebikes -o lwc.demo
+sf project deploy start
+sf org assign permset -n ebikes
+sf org assign permset -n Walkthroughs
+sf data tree import -p ./data/sample-data-plan.json
+sf community publish -n E-Bikes
+sf project deploy start --metadata-dir=guest-profile-metadata -w 10
+sf org open
