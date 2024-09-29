986e85bf8ee4c05260887095fcb5d38bde96e344
zhou059 <zhou059@gmail.com>
2024-09-28 22:30:04 -0700

    make it trialed

--- orgInit.sh
+++ orgInit.sh
@@ -1,17 +1,19 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 5 -s --wait 60 --userprefix ebikes -o lwc.demo
+sfdx shane:org:create -f config/project-scratch-def.json -d 5 -s --wait 60 --userprefix dxp -o work.shop
 
 # npm install
 # node run.js
 # sleep 120
 
-sfdx force:source:push
-sf org assign permset -n ebikes
-sf org assign permset -n Walkthroughs
+# sfdx force:source:push
+# sf org assign permset -n ebikes
+# sf org assign permset -n Walkthroughs
 # sfdx force:user:permset:assign -n EinsteinGPTPromptTemplateManager
 
 # sf data tree import -p ./data/sample-data-plan.json
-sf automig load -d data
-sf community publish -n E-Bikes
-sf project deploy start --metadata-dir=guest-profile-metadata -w 10
+# sf automig load -d data
+# sf community publish -n E-Bikes
+# sf project deploy start --metadata-dir=guest-profile-metadata -w 10
+
+sfdx force:source:deploy -p trial-app
 sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:org:open
