5b90e23b6caada49a2e96747ec2ccebb10ac9686
zhou059 <zhou059@gmail.com>
2024-09-29 22:11:56 -0700

    make it trialed

--- orgInit.sh
+++ orgInit.sh
@@ -15,5 +15,5 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --us
 # sf project deploy start --metadata-dir=guest-profile-metadata -w 10
 
 sfdx force:source:deploy -p trial-app
-sfdx shane:user:password:set -p salesforce1 -g User -l User
+# sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:org:open
