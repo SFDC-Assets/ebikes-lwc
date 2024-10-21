daf4d8d364d4c04a3e665d4da2b146b9f00351e6
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-21 12:38:55 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix dxp -o work.shop
+sf demoutil org create scratch -f config/project-scratch-def.json -d 30 -s -p dxp -e wrok.shop
 
 # npm install
 # node run.js
@@ -14,6 +14,6 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --us
 # sf community publish -n E-Bikes
 # sf project deploy start --metadata-dir=guest-profile-metadata -w 10
 
-sfdx force:source:deploy -p trial-app
-sfdx shane:user:password:set -p expcloud1 -g User -l User
-sfdx force:org:open
+sf project deploy start -p trial-app
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org open
